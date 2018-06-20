2587c82fa192824754c492f0d029d43c6c90fbda
Jay Zuo <yjzuo6@gmail.com>
2018-06-20 14:17:53 -0400

    Add credit to ivan@sanchezortega.es

--- L.Polyline.SnakeAnim.js
+++ L.Polyline.SnakeAnim.js
@@ -211,6 +211,12 @@ L.LayerGroup.mergeOptions({
 });
 
 
+/*"THE BEER-WARE LICENSE":
+<ivan@sanchezortega.es> wrote this file. As long as you retain this notice you
+can do whatever you want with this stuff. If we meet some day, and you think
+this stuff is worth it, you can buy me a beer in return.*/
+
+
 
 
 
