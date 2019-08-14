51f09a8a4d1e6c6e143816823e259e3ae5a25155
Pitou <pitou.games@gmail.com>
2019-08-14 23:55:48 +0200

    Add snakeOut() method (only for polylines)

--- L.Polyline.SnakeAnim.js
+++ L.Polyline.SnakeAnim.js
@@ -5,7 +5,6 @@ this stuff is worth it, you can buy me a beer in return.*/
 
 ///// FIXME: Use path._rings instead of path._latlngs???
 ///// FIXME: Panic if this._map doesn't exist when called.
-///// FIXME: Implement snakeOut()
 ///// FIXME: Implement layerGroup.snakeIn() / Out()
 
 
@@ -19,20 +18,24 @@ L.Polyline.include({
 	// Yeah, yeah, "rings" semantically only apply to polygons, but L.Polyline
 	// internally uses that nomenclature.
 	_snakingRings: 0,
+	_snakingTailRings: 0,
 	_snakingVertices: 0,
+	_snakingTailVertices: 0,
 
 	// Distance to draw (in screen pixels) since the last vertex
 	_snakingDistance: 0,
+	_snakingTailDistance: 0,
 
-	// Flag
-	_snaking: false,
+	// Flags
+	_snakingIn: false,
+	_snakingOut: false,
 
 
 	/// TODO: accept a 'map' parameter, fall back to addTo() in case
 	/// performance.now is not available.
 	snakeIn: function(){
 
-		if (this._snaking) { return; }
+		if (this._snakingIn || this._snakingOut) { return; }
 
 		if ( !('performance' in window) ||
 		     !('now' in window.performance) ||
@@ -40,7 +43,7 @@ L.Polyline.include({
 			return;
 		}
 
-		this._snaking = true;
+		this._snakingIn = true;
 		this._snakingTime = performance.now();
 		this._snakingVertices = this._snakingRings = this._snakingDistance = 0;
 
@@ -56,29 +59,91 @@ L.Polyline.include({
 
 		this._update();
 		this._snake();
-		this.fire('snakestart');
+		this.fire('snakestart'); // to depreciate
+		this.fire('snakeInStart');
 		return this;
 	},
 
+	snakeOut: function(){
+
+		if (this._snakingOut) { return; }
+
+		if ( !('performance' in window) ||
+			!('now' in window.performance) ||
+			!this._map) {
+			return;
+		}
+
+		this._snakingOut = true;
+		this._snakingTime = performance.now();
+		this._snakingTailVertices = this._snakingTailRings = this._snakingTailDistance = 0;
+
+		if (!this._snakeLatLngs) {
+			this._snakeLatLngs = L.LineUtil.isFlat(this._latlngs) ?
+				[ this._latlngs ] :
+				this._latlngs ;
+		}
+
+		if(!this._snakingIn){
+			//need to do a deep copy
+			var tempArray = [];
+			var keys = Object.keys(this._snakeLatLngs);
+			for (var i in keys) {
+				tempArray.push([]);
+				this._snakeLatLngs[tempArray.length - 1].forEach(function(entry) {
+					tempArray[tempArray.length - 1].push( [entry.lat, entry.lng] );
+				});
+			}
+			this._latlngs = tempArray;
+		}
+
+		this._update();
+		// Avoid concourant calls to _snake
+		if(!this._snakingIn){
+			this._snake();
+		}
+		this.fire('snakeOutStart');
+		return this;
+	},
 
 	_snake: function(){
+		// If polyline has been removed from the map stop _snakeForward
+		if (!this._map) return;
 
 		var now = performance.now();
 		var diff = now - this._snakingTime;	// In milliseconds
-		diff = (diff === 0 ? 0.001 : diff) // avoids low time resolution issues in some browsers
+		diff = (diff === 0 ? 0.001 : diff); // avoids low time resolution issues in some browsers
 		var forward = diff * this.options.snakingSpeed / 1000;	// In pixels
 		this._snakingTime = now;
 
 		// Chop the head from the previous frame
-		this._latlngs[ this._snakingRings ].pop();
+		if(this._snakingIn){
+			this._latlngs[ this._snakingRings ].pop();
+		}
+		// Chop the tail from the previous frame
+		if(this._snakingOut){
+			this._latlngs[ this._snakingTailRings ].shift();
+		}
 
-		return this._snakeForward(forward);
+		if(this._snakingIn){
+			this._snakeHeadForward(forward);
+		}
+		if(this._snakingOut){
+			this._snakeTailForward(forward);
+		}
+
+		this.setLatLngs(this._latlngs);
+		// Animate only if snake in moving
+		if (this._snakingIn || this._snakingOut){
+			this.fire('snake');
+			L.Util.requestAnimFrame(this._snake, this);
+		}
+
+		return this;
 	},
 
-	_snakeForward: function(forward) {
+	_snakeHeadForward: function(forward) {
 
-		// If polyline has been removed from the map stop _snakeForward
-		if (!this._map) return;
 		// Calculate distance from current vertex to next vertex
 		var currPoint = this._map.latLngToContainerPoint(
 			this._snakeLatLngs[ this._snakingRings ][ this._snakingVertices ]);
@@ -87,17 +152,17 @@ L.Polyline.include({
 
 		var distance = currPoint.distanceTo(nextPoint);
 
-//		console.log('Distance to next point:', distance, '; Now at: ', this._snakingDistance, '; Must travel forward:', forward, '_snakingTime', this._snakingTime, '_snakingVertices', this._snakingVertices);
-//		console.log('Snake vertices: ', this._latlngs,';this._snakeLatLngs',this._snakeLatLngs);
+		//console.log('Distance head to next point:', distance, '; Now at: ', this._snakingDistance, '; Must travel forward:', forward, '_snakingTime', this._snakingTime, '_snakingVertices', this._snakingVertices);
+		//console.log('Snake vertices: ', this._latlngs,';this._snakeLatLngs',this._snakeLatLngs);
 
-		if (this._snakingDistance + forward > distance) {
+		while (this._snakingDistance + forward > distance) {
 			// Jump to next vertex
 			this._snakingVertices++;
 			this._latlngs[ this._snakingRings ].push( this._snakeLatLngs[ this._snakingRings ][ this._snakingVertices ] );
 
 			if (this._snakingVertices >= this._snakeLatLngs[ this._snakingRings ].length - 1 ) {
 				if (this._snakingRings >= this._snakeLatLngs.length - 1 ) {
-					return this._snakeEnd();
+					return this._snakeInEnd();
 				} else {
 					this._snakingVertices = 0;
 					this._snakingRings++;
@@ -108,7 +173,11 @@ L.Polyline.include({
 			}
 
 			this._snakingDistance -= distance;
-			return this._snakeForward(forward);
+			currPoint = this._map.latLngToContainerPoint(
+				this._snakeLatLngs[ this._snakingRings ][ this._snakingVertices ]);
+			nextPoint = this._map.latLngToContainerPoint(
+				this._snakeLatLngs[ this._snakingRings ][ this._snakingVertices + 1]);
+			distance = currPoint.distanceTo(nextPoint);
 		}
 
 		this._snakingDistance += forward;
@@ -126,16 +195,85 @@ L.Polyline.include({
 			this._map.setView(headLatLng);
 		}
 
-		this.setLatLngs(this._latlngs);
-		this.fire('snake');
-		L.Util.requestAnimFrame(this._snake, this);
+		return this;
 	},
 
-	_snakeEnd: function() {
+	_snakeTailForward: function(forward) {
+		// Calculate distance from current vertex to next vertex
+		var currPoint = this._map.latLngToContainerPoint(
+			this._snakeLatLngs[ this._snakingTailRings ][ this._snakingTailVertices ]);
+		var nextPoint = this._map.latLngToContainerPoint(
+			this._snakeLatLngs[ this._snakingTailRings ][ this._snakingTailVertices + 1 ]);
 
-		this.setLatLngs(this._snakeLatLngs);
-		this._snaking = false;
-		this.fire('snakeend');
+		var distance = currPoint.distanceTo(nextPoint);
+
+		//console.log('Distance tail to next point:', distance, '; Now at: ', this._snakingTailDistance, '; Must travel forward:', forward, '; _snakingTime', this._snakingTime, '; _snakingTailVertices', this._snakingTailVertices);
+		//console.log('Snake vertices: ', this._latlngs,';this._snakeLatLngs',this._snakeLatLngs);
+
+		while (this._snakingTailDistance + forward > distance) {
+			// Jump to next vertex
+			this._snakingTailVertices++;
+			this._latlngs[this._snakingTailRings].shift();
+
+			if (this._snakingTailVertices >= this._snakeLatLngs[ this._snakingTailRings ].length - 1 ) {
+				if (this._snakingTailRings >= this._snakeLatLngs.length - 1 ) {
+					return this._snakeOutEnd();
+				} else {
+					this._snakingTailVertices = 0;
+					this._latlngs[ this._snakingTailRings ] = [];
+					this._snakingTailRings++;
+					this._latlngs[ this._snakingTailRings ].shift(); // Remove first point of new line
+				}
+			}
+
+			this._snakingTailDistance -= distance;
+			currPoint = this._map.latLngToContainerPoint(
+				this._snakeLatLngs[ this._snakingTailRings ][ this._snakingTailVertices ]);
+			nextPoint = this._map.latLngToContainerPoint(
+				this._snakeLatLngs[this._snakingTailRings ][ this._snakingTailVertices + 1 ]);
+			distance = currPoint.distanceTo(nextPoint);
+		}
+
+		this._snakingTailDistance += forward;
+
+		var percent = this._snakingTailDistance / distance;
+
+		var tailPoint = nextPoint.multiplyBy(percent).add(
+			currPoint.multiplyBy( 1 - percent )
+		);
+
+		// Put a new tail in place.
+		var tailLatLng = this._map.containerPointToLatLng(tailPoint);
+		this._latlngs[ this._snakingTailRings ].unshift(tailLatLng);
+
+		return this;
+	},
+
+
+	_snakeInEnd: function() {
+
+		this._snakingIn = false;
+		if(!this._snakingOut){
+			this.setLatLngs(this._snakeLatLngs);
+		}
+		this.fire('snakeend'); // to depreciate
+		this.fire('snakeInEnd');
+
+	},
+
+	_snakeOutEnd: function() {
+
+		this._snakingOut = false;
+		this.fire('snakeOutEnd');
+
+	},
+
+	_snakeReset: function () {
+
+		this._snakingIn = this._snakingOut = false;
+		if(this._snakeLatLngs){
+			this.setLatLngs(this._snakeLatLngs);
+		}
 
 	}
 
--- README.md
+++ README.md
@@ -19,6 +19,12 @@ Current version works only with Leaflet 1.1 or higher.
 New method in both `L.Polyline` and `L.LayerGroup`: `snakeIn()`. Call it to
 trigger the animation.
 
+New method `L.Polyline` : `snakeOut()`. Call it to
+trigger the animation. You can call it during the `snakeIn()` animation.
+
+New method `L.Polyline` : `snakeReset()`. Call it to
+stop any animation and bring back polyline.
+
 New option in `L.Polyline`: `snakingSpeed`. This is the speed of the animation,
 in pixels per second. Pixels refer to the length of the polyline at the current
 zoom level. The default value is `200`.
@@ -27,11 +33,14 @@ New option in `L.Polyline`: `followHead`. This is to keep the map centered
 on the head. The default value is `false`.
 
 New option in `L.LayerGroup`: `snakingPause`. This is the number of milliseconds
-to wait between layers in the group when doing a snaking animation.
+to wait between layers in the group when doing a snaking animation. The default 
+value is `200`.
 
 ```js
-var line = L.polyline(latlngs, {snakingSpeed: 200});
+var line = L.polyline(latlngs, {snakingSpeed: 200, followHead: false});
 line.addTo(map).snakeIn();
+// Later, you can use
+line.snakeOut();
 ```
 
 ```js
@@ -44,7 +53,8 @@ route.addTo(map).snakeIn();
 ```
 
 When a polyline or layer group is performing the snaking animation, it will
-fire `snakestart`, `snake` and `snakeend` events.
+fire `snakeInStart` (head start), `snakeOutStart` (tail start),`snake`,
+`snakeInEnd` (head end) and `snakeOutEnd` (tail end) events.
 
 ### Legalese
 
